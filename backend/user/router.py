180c7bf4fbd853ec05567328eaafb73a0766a57a
Milansuman <milansuman2016@gmail.com>
2026-06-30 10:17:43 +0530

    fix: Ensure dependency and route do not conflict

--- backend/user/router.py
+++ backend/user/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from auth.dependencies import get_current_user
+from auth.dependencies import get_current_user as get_current_user_dep
 from auth.schema import TokenPayload
 from db.connection import get_db
 from exceptions.exceptions import UnauthorizedException
@@ -24,7 +24,7 @@ router = APIRouter(
 )
 async def get_current_user(
     db: AsyncSession = Depends(get_db),
-    current_user: TokenPayload = Depends(get_current_user)
+    current_user: TokenPayload = Depends(get_current_user_dep)
 ):
     return await user_service.get_user_by_id(
         id=int(current_user.sub),
@@ -39,7 +39,7 @@ async def get_current_user(
 async def create_user(
     body: UserCreate,
     db: AsyncSession = Depends(get_db),
-    current_user: TokenPayload = Depends(get_current_user),
+    current_user: TokenPayload = Depends(get_current_user_dep),
 ):
     if not current_user.is_admin:
         raise UnauthorizedException("Action not allowed")
@@ -93,7 +93,7 @@ async def patch_user(
     id: int,
     body: UserUpdate,
     db: AsyncSession = Depends(get_db),
-    current_user: TokenPayload = Depends(get_current_user),
+    current_user: TokenPayload = Depends(get_current_user_dep),
 ):
     if str(id) != current_user.sub and not current_user.is_admin:
         raise UnauthorizedException("Action not allowed")
@@ -114,7 +114,7 @@ async def patch_user(
 async def delete_user(
     id: int,
     db: AsyncSession = Depends(get_db),
-    current_user: TokenPayload = Depends(get_current_user),
+    current_user: TokenPayload = Depends(get_current_user_dep),
 ):
     if str(id) != current_user.sub and not current_user.is_admin:
         raise UnauthorizedException("Action not allowed")
